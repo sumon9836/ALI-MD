2ad3b6dabd2038ab6cd98730e2d169240e388788
sumon9836 <panelbot10@gmail.com>
2025-05-23 23:29:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const {
   const mode = config.MODE
   const online = config.ALWAYS_ONLINE
   const status = config.AUTO_STATUS_SEEN
-  const ownerNumber = ['923003588997']
+  const ownerNumber = ['918348672970']
   
   const tempDir = path.join(os.tmpdir(), 'cache-temp')
   if (!fs.existsSync(tempDir)) {
